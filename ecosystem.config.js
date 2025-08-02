7fc1092eb4283285de95f8b1dc20d8f9a0be8843
HichOoMa <haitham.benhammouda@gmail.com>
2025-08-02 12:29:07 +0100

    feat: remove environment variables and logging configurations from ecosystem setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,7 @@ module.exports = {
       script: 'npm run build && npm start',
       watch: false,
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000
-      },
       log_date_format: 'YYYY-MM-DD HH:mm Z',
-      error_file: './logs/err.log',
-      out_file: './logs/out.log',
-      log_file: './logs/combined.log',
       time: true
     }
   ]
