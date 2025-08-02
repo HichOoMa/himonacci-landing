a7b83ded2d7055a9ef6d313365e06936f5ead1af
HichOoMa <haitham.benhammouda@gmail.com>
2025-08-02 12:11:15 +0100

    feat: add ecosystem configuration for production deployment of the dashboard

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+export const apps = [
+  {
+    name: 'himonacci-dashboard',
+    script: 'npm',
+    args: 'run build && npm start',
+    cwd: '/home/hichoma/Dev/hammem/dashboard',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }
+];
