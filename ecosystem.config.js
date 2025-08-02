7be57268f0d98c94e06c31208ae3094777648d9a
HichOoMa <haitham.benhammouda@gmail.com>
2025-08-02 12:28:11 +0100

    feat: update ecosystem configuration to use module.exports and rename application to 'himonacci-dashboard'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,19 @@
-export const apps = [
-  {
-    name: 'dashboard',
-    script: 'npm run build && npm start',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
-    log_date_format: 'YYYY-MM-DD HH:mm Z',
-    time: true
-  }
-];
+module.exports = {
+  apps: [
+    {
+      name: 'dashboard',
+      script: 'npm run build && npm start',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }
+  ]
+};
