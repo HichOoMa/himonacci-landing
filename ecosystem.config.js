a1c69a4cf4970e456df3c3d70aa9d90644b3f278
HichOoMa <haitham.benhammouda@gmail.com>
2025-08-02 12:23:06 +0100

    feat: remove logging file configurations from ecosystem setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,9 +13,6 @@ export const apps = [
       PORT: 3000
     },
     log_date_format: 'YYYY-MM-DD HH:mm Z',
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
     time: true
   }
 ];
